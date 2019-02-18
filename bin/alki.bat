7015b69f89e6bd962d891de452a4f8e07ff69717
dlucanu <dlucanu@info.uaic.ro>
2019-02-18 12:41:03 +0200

    updated alki.bat to allow paths with spaces

--- bin/alki.bat
+++ bin/alki.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 
+SET jarpath=%~dp0
 IF %1.==. GOTO No1
 IF %2.==. GOTO No2
- java -jar %~dp0alk.jar -a %1 -i %2
+ java -jar "%jarpath%alk.jar" -a %1 -i %2
 GOTO End1
 
 :No1
@@ -12,7 +13,7 @@ GOTO End1
   ECHO "alki <file-with-algorithm>.alk <file-with-initial-state>.in"
 GOTO End1
 :No2
-  java -jar %~dp0alk.jar -a %1 
+  java -jar "%jarpath%alk.jar" -a %1 
 GOTO End1
 
 :End1
