84b712132d2f226aaa082d9911d8b7792f9bf901
Alexandru Lungu <lungualex00@gmail.com>
2019-04-09 13:09:59 +0300

    added script in C alki.out

---
+++ script/alki.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char* args[])
+{
+    char* ars[argc+3];
+    char *ar0 = "-jar";
+    char *ar1 = "alk.jar";
+    ars[0]="java";
+    ars[1]=ar0;
+    ars[2]=ar1;
+    for (int i=1; i<argc; i++)
+        ars[i+2]=args[i];
+    ars[argc+2] = NULL;
+    execvp("java", ars);
+    return 0;
+}
