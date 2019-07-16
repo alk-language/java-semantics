d24910305901d68308db5f53145a7c3b99f7c9d0
Alexandru Lungu <lungualex00@gmail.com>
2019-07-16 23:24:31 +0300

    implemented options

--- src/main/java/impl/constants/Constants.java
+++ src/main/java/impl/constants/Constants.java
@@ -1,7 +1,8 @@
 package impl.constants;
 
 public class Constants {
-    public final static int MAX_DECIMALS = 10;
-    public final static int MAX_ARRAY = 1000000000;
+    public static final String VERSION = "v1.0";
+    public static int MAX_DECIMALS = 10;
+    public static int MAX_ARRAY = 1000000000;
     public final static boolean DEBUG = false;
 }
