26add7316533ca13323ea79b46593c4c4c1c96b5
lungualex00@gmail.com <2xbunicutule>
2021-02-15 17:37:21 +0200

    minor fix

--- bin/alki.sh
+++ bin/alki.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-jardir="$(dirname "$BASH_SOURCE")"
-#jardir="$(dirname "$(readlink -f "$0")")"
-#echo $jardir
+TARGET_FILE=$0
 
-java -jar $jardir/alk.jar ${@:1}
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+
+# Compute the canonicalized name by finding the physical path
+# for the directory we're in and appending the target file.
+PHYS_DIR=`pwd -P`
+echo $PHYS_DIR/alk.jar
+
+java -jar $PHYS_DIR/alk.jar ${@:1}
 exit 0
