95a88643ca11ef4d5f96008d3c14233cda0bfcd0
lungualex00@gmail.com <lungualex00@gmail.com>
2021-02-15 15:33:53 +0200

    minor fix

--- bin/alki.sh
+++ bin/alki.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#jardir="$(dirname "$BASH_SOURCE")"
-jardir="$(dirname "$(readlink -f "$0")")"
+jardir="$(dirname "$BASH_SOURCE")"
+#jardir="$(dirname "$(readlink -f "$0")")"
 #echo $jardir
 
 java -jar $jardir/alk.jar ${@:1}
