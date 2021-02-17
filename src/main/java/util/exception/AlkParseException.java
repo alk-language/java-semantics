1e12a5ad117c286353f1f4e48851cc35cec09480
lungualex00@gmail.com <2xbunicutule>
2021-02-17 16:21:51 +0200

    handle parse exceptions

--- src/main/java/util/exception/AlkParseException.java
+++ src/main/java/util/exception/AlkParseException.java
@@ -1,21 +1,16 @@
 package util.exception;
 
-import org.apache.commons.cli.ParseException;
+import execution.parser.exceptions.AlkException;
 
 /**
  * Exeption which should be thrown in case a file was not found
  * This is a runtime exception, so it can be used to wrap IO exceptions
  */
-public class AlkParseException extends InternalException
+public class AlkParseException
+extends AlkException
 {
-    /**
-     * Wrapper constructor
-     *
-     * @param e
-     * The parsing exception which should be wrapped
-     */
-    public AlkParseException(ParseException e)
+    public AlkParseException(String text)
     {
-        super(e);
+        super(text);
     }
 }
