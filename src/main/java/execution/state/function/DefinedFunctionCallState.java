a934b72f7cd5239391d25516e0dfc5d5ea8324a0
lungualex00@gmail.com <lungualex00@gmail.com>
2021-03-07 18:50:27 +0200

    fixed undefined variable exception

--- src/main/java/execution/state/function/DefinedFunctionCallState.java
+++ src/main/java/execution/state/function/DefinedFunctionCallState.java
@@ -74,6 +74,10 @@ extends ExecutionState
                 }
                 else
                 {
+                    if (params.get(i).toRValue() == null || params.get(i).toRValue() instanceof AlkNotAValue)
+                    {
+                        super.handle(new AlkException("Undefined variable used as parameter."));
+                    }
                     env.update(param.getName(), params.get(i).toRValue().clone(generator));
                 }
             }
