a6d402c9ab06c0d8853a2bb3a88c46a7ec022226
Alex Lungu <lungualex00@gmail.com>
2022-02-21 21:11:17 +0200

    changed version

--- src/main/java/execution/parser/constants/Constants.java
+++ src/main/java/execution/parser/constants/Constants.java
@@ -2,7 +2,7 @@ package execution.parser.constants;
 
 public class Constants
 {
-    public static final String VERSION = "3.2";
+    public static final String VERSION = "4.0";
     public static int MAX_DECIMALS = 10;
     public static int MAX_ARRAY = 1000000000;
     public final static boolean DEBUG = false;
