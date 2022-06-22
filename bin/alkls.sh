231b7bd93a97135caea7563b86e1d67160ab5a4c
vladvlad00 <teodorescu.vlad@yahoo.com>
2022-06-22 18:59:08 +0300

    script for linux

---
+++ bin/alkls.sh
@@ -0,0 +1,26 @@
+	#!/bin/bash
+
+TARGET_FILE=$0
+ORIG=`pwd -P`
+
+
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+
+# Compute the canonicalized name by finding the physical path
+# for the directory we're in and appending the target file.
+PHYS_DIR=`pwd -P`
+cd "$ORIG"
+export PATH="$PATH:$PHYS_DIR:$PHYS_DIR/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PHYS_DIR:$PHYS_DIR/lib"
+export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$PHYS_DIR:$PHYS_DIR/lib"
+java -Djava.library.path="$PHYS_DIR:$PHYS_DIR/lib" -cp "$PHYS_DIR/alk.jar:$PHYS_DIR/lib/com.microsoft.z3.jar" main.LanguageServer "${@:1}"
+exit 0
